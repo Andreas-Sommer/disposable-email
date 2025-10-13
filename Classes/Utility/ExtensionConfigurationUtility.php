35449df15732e43a91108e2d94a045b4154f1cc9
Andreas Sommer <sommer@belsignum.com>
2025-10-13 10:26:41 +0200

    Fix typo in variable name `$poermailLoaded` to `$powermailLoaded` in `ExtensionConfigurationUtility`

--- Classes/Utility/ExtensionConfigurationUtility.php
+++ Classes/Utility/ExtensionConfigurationUtility.php
@@ -6,13 +6,13 @@ class ExtensionConfigurationUtility
 {
     public static function isOverloadEmailValidationActive(): bool
     {
-        $poermailLoaded = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('powermail');
+        $powermailLoaded = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('powermail');
         $overloadEmailValidation = (bool)(\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
             \TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class
         )->get('disposable_email')['overloadEmailValidation']);
 
         return (
-            $poermailLoaded === true                // powermail loaded
+            $powermailLoaded === true                // powermail loaded
             && $overloadEmailValidation === true    // but overloadEmailValidation === true
         );
     }
